ceb1252ff88a71d33de4c32301229c1be3a62923
Darkos <loboda.deni@gmail.com>
2020-10-03 20:24:27 +0300

    progress

--- src/commonMain/kotlin/com/darkos/mvu/validation/DSL.kt
+++
@@ -1,4 +0,0 @@
-package com.darkos.mvu.validation
-
-fun ValidationHandler(block: ValidationEffectHandler.Builder.() -> Unit): ValidationEffectHandler =
-    ValidationEffectHandler.Builder().apply(block).build()
