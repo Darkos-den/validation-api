bbdb82611c4400a191794b395d6a52f3ae4d0bcd
Darkos <loboda.deni@gmail.com>
2020-11-15 15:36:02 +0300

    update

--- src/commonMain/kotlin/com/darkos/mvu/validation/IValidationHandler.kt
+++ src/commonMain/kotlin/com/darkos/mvu/validation/IValidationHandler.kt
@@ -1,5 +1,11 @@
 package com.darkos.mvu.validation
 
 import com.darkos.mvu.EffectHandler
+import com.darkos.mvu.models.flow.FlowEffect
+import com.darkos.mvu.models.flow.FlowMessage
 
-interface IValidationHandler : EffectHandler
+interface IValidationHandler : EffectHandler {
+    override suspend fun call(effect: FlowEffect): kotlinx.coroutines.flow.Flow<FlowMessage> {
+        throw IllegalStateException("this handler does not support calling as Flow")
+    }
+}
